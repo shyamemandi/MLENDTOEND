e47e60d73919ee81efc332fcfd27bbaa0f3fa836
shyamemandi <shyamgis777@gmail.com>
2024-12-25 12:36:48 -0600

    Data Ingestion code and ipynb

--- src/logger.py
+++ src/logger.py
@@ -2,6 +2,7 @@ import os
 import logging
 from datetime import datetime
 
+
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%y_%H_%M_%S')}.log"
 logs_path = os.path.join(os.getcwd(),"logs", LOG_FILE)
 
@@ -18,5 +19,5 @@ logging.basicConfig(
 )
 
 
-if __name__ == "__main__":
-    logging.info("Logging has started")
+#if __name__ == "__main__":
+    #logging.info("Logging has started")
